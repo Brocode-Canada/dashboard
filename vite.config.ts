fd0a443e13725eec0bf8ce2339408085d2aca778
Hamza Siddiqui <hamzahmedsiddiqui@outlook.com>
2025-07-14 01:27:49 -0400

    Fix security warnings and GitHub Actions environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/dashboard/' : '/',
+  server: {
+    fs: {
+      strict: true,
+      allow: ['..']
+    }
+  }
 })
